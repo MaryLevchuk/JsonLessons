c57020cf62789da2c296d78c8dbf8e351321978d
mary <marylevchuk@gmail.com>
2017-01-06 22:33:12 +0200

    Initial commit

---
+++ JsonLessons/JsonLessons/TypedAccess/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonLessons
+{
+    public class TypedAccessTests
+    {
+
+        public TypedAccessTests()
+        {
+            this.SimpleObject = new SimpleObject
+            {
+                Boolean = true,
+                DateTime = DateTime.Now,
+                Decimal = 10,
+                Int = 1,
+                NestedObject = new SimpleObject(),
+                NullableBoolean = true,
+                Object = new SimpleObject(),
+                String = "string field",
+                StringArray = {"string1", "string2", "string3"}
+            };
+
+            SimpleObject.Money = 1.0M;
+        }
+
+        public SimpleObject SimpleObject { get; set; }
+
+        [Test]
+        public void FirstTest()
+        {
+            string jsonString = JsonConvert.SerializeObject(SimpleObject);
+            Console.WriteLine("jsonString = {0}", jsonString);
+        }
+    }
+}
---
+++ JsonLessons/JsonLessons/TypedAccess/SimpleObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonLessons
+{
+    public class SimpleObject
+    {
+        public string String { get; set; }
+
+        public int Int { get; set; }
+
+        public decimal Decimal { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public bool Boolean { get; set; }
+
+        public bool? NullableBoolean { get; set; } 
+
+        public object Object { get; set; }
+
+        public List<string> StringArray { get; set; }
+
+        public SimpleObject NestedObject { get; set; }
+
+        public decimal Money { get; set; }
+    }
+}
